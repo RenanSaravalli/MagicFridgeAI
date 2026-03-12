a86362102e021f4dbb6d07d40fcd05862bffad6b
RenanSampaioSaravalli <renan.saravalli@aluno.ifsp.edu.br>
2026-03-12 17:58:46 -0300

    fix: Adiciona atributos para enum de Category

--- src/main/java/dev/java10x/MagicFridgeAI/model/Category.java
+++ src/main/java/dev/java10x/MagicFridgeAI/model/Category.java
@@ -1,4 +1,19 @@
 package dev.java10x.MagicFridgeAI.model;
 
 public enum Category {
+    FRUIT,
+    VEGETABLE,
+    MEAT,
+    FISH_SEAFOOD,
+    DAIRY,
+    GRAIN_CEREAL,
+    LEGUME,
+    BEVERAGE,
+    SNACK,
+    SPICE_SEASONING,
+    SAUCE_CONDIMENT,
+    BAKERY,
+    FROZEN,
+    SWEET_DESSERT,
+    OTHER
 }
