870b427e5bbe580888d324c19afea02f260a1d0a
RenanSampaioSaravalli <renan.saravalli@aluno.ifsp.edu.br>
2026-03-13 18:33:19 -0300

    Feat: Adiciona dependencias do geminiService e cria metodo de get para a url

--- src/main/java/dev/java10x/MagicFridgeAI/controller/RecipeController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/RecipeController.java
@@ -1,4 +1,27 @@
 package dev.java10x.MagicFridgeAI.controller;
 
+import dev.java10x.MagicFridgeAI.service.GeminiService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
 public class RecipeController {
+
+    private GeminiService geminiService;
+
+    public RecipeController(GeminiService geminiService) {
+        this.geminiService = geminiService;
+    }
+
+    /*Métod de requisição ao gemini espera o prompt de resposta da (string)*/
+    @GetMapping
+    public Mono<ResponseEntity<String>> generateRecipe() {
+
+        return geminiService.generateRecipe();
+
+    }
+
+
 }
